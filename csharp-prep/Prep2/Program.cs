54a08d6540fae8c7eb4a3b3d529d76e9e7df69ba
earlalay <earlpearlalayna@gmail.com>
2026-09-18 21:21:09 -0600

    Did the core requirements for the letter grade assignment for Prep2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What was your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradeNum = int.Parse(userInput);
+
+        string letter = "Z";
+
+        if (gradeNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter == "A" || letter == "F")
+        {
+            Console.WriteLine($"You got an {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}.");
+        }
+
+        if (gradeNum >= 70)
+        {
+            Console.Write("You passed the class!");
+        }
+        else
+        {
+            Console.Write("You didn't pass, but I know you can get there!");
+        }
+
     }
 }
