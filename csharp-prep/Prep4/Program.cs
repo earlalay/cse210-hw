e42d821f61c7a7013667a1c655a981ff4afafe91
earlalay <earlpearlalayna@gmail.com>
2026-09-25 22:04:14 -0600

    Worked with lists and math.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int currentNumber = -1;
+        while (currentNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userNumber = Console.ReadLine();
+            currentNumber = int.Parse(userNumber);
+            if (currentNumber != 0)
+            {
+                numbers.Add(currentNumber);
+            }
+        }
+        
+        int numberTotal = 0;
+        int largestNumber = 1;
+        foreach (int number in numbers)
+        {
+            numberTotal += number;
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+        }
+        double numberAverage = (double)numberTotal / numbers.Count;
+        Console.WriteLine($"The sum is: {numberTotal}");
+        Console.WriteLine($"The average is: {numberAverage}");
+        Console.Write($"The largest number is: {largestNumber}");
     }
 }
