bd0434050413542e7e97957bb08e5a93f49556c7
earlalay <earlpearlalayna@gmail.com>
2026-09-25 22:55:52 -0600

    Practiced writing and calling functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,63 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNUmber();
+        int birthYear = PromptUserBirthYear();
+        int squareNumber = SquareNumber(number);
+        int ageThisYear = 2026 - birthYear;
+
+        DisplayResult(name, squareNumber, ageThisYear);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNUmber()
+        {
+            Console.Write("What is your favorite whole number? ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            return number;
+        }
+
+        static int PromptUserBirthYear()
+        {
+            Console.Write("What is your birth year? ");
+            string userInput = Console.ReadLine();
+            int birthYear = int.Parse(userInput);
+            return birthYear;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squareNumber = number * number;
+            return squareNumber;
+        }
+
+        static void DisplayResult(string name, int squareNumber, int ageThisYear)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squareNumber}.");
+            Console.Write($"{name}, you will turn {ageThisYear} this year.");
+        }
+
     }
 }
+
+
+// static returnType FunctionName(dataType parameter1, dataType parameter2)
+// {
+//     // function_body
+// }
